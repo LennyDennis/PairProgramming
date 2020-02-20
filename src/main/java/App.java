d48c76ab5a05ebc2c5c4ab136b84bae5fdfe69a6
LennyDennis <lennydennis@gmail.com>
2020-02-20 14:53:52 +0300

    Add postgres dependencies in the build gradle

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,12 +3,16 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import org.sql2o.Sql2o;
+import org.sql2o.Connection;
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        String connectionString = "jdbc:postgresql://localhost:5432/paired";
+        Sql2o sql2o = new Sql2o(connectionString, "lenny", " ");
+
         Map<String, Object> model = new HashMap<String, Object>();
 
         get("/", (req, res) -> {
