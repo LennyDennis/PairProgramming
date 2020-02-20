63e68ec1d96b6d77b6828250eda09664ed53cee8
LennyDennis <lennydennis@gmail.com>
2020-02-20 15:29:15 +0300

    Add the post route to create a new students

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import dao.Sql2oStudentsDao;
+import models.Students;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -9,18 +11,29 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        Sql2oStudentsDao studentsDao;
+
         staticFileLocation("/public");
         String connectionString = "jdbc:postgresql://localhost:5432/paired";
         Sql2o sql2o = new Sql2o(connectionString, "lenny", " ");
 
+        studentsDao = new Sql2oStudentsDao(sql2o);
+
         Map<String, Object> model = new HashMap<String, Object>();
 
         get("/", (req, res) -> {
+            model.put("students", studentsDao.getAllStudents());
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
         get("/pairs", (req, res) -> {
             return new ModelAndView(model, "paired.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/students/new",(req,res) ->{
+            String name = req.queryParams("studentName");
+            Students student = new Students(name);
+            return new ModelAndView(model,"index.hbs");
         }, new HandlebarsTemplateEngine());
     }
 
