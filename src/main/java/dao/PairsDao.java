8578671f9035855f622a9a4ad5bc7edc5a51f72f
timothy12maisha <timomaisha@gmail.com>
2020-02-20 18:00:52 +0300

    Add PairsDao and Sql2o

--- src/main/java/dao/PairsDao.java
+++ src/main/java/dao/PairsDao.java
@@ -1,4 +1,18 @@
 package dao;
 
+import models.Pairs;
+
+import java.util.List;
+
 public interface PairsDao {
+    //create
+    void addPairs(Pairs newPairs);
+
+
+
+    //
+    List<Pairs> getAllPairs();
+    List<Pairs> getStudent1();
+    List<Pairs> getStudent2();
+
 }
