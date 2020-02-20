2d63726c83e93150afb59477209093fde8bb8eab
LennyDennis <lennydennis@gmail.com>
2020-02-20 15:32:24 +0300

    Implement student class methods

--- src/main/java/dao/Sql2oStudentsDao.java
+++ src/main/java/dao/Sql2oStudentsDao.java
@@ -1,4 +1,38 @@
 package dao;
 
-public class Sql2oStudentsDao {
+import models.Students;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class Sql2oStudentsDao implements StudentsDao{
+
+    private final Sql2o sql2o;
+
+    public Sql2oStudentsDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void addStudent(Students student){
+        String sql = "INSERT INTO students (name,studentDate) VALUES (:name,now())";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql,true)
+                    .bind(student)
+                    .executeUpdate()
+                    .getKey();
+            student.setId(id);
+        }
+
+    }
+
+    @Override
+    public List<Students> getAllStudents(){
+        String sql="SELECT * FROM students";
+        try(Connection con = sql2o.open()){
+            return con.createQuery(sql)
+                    .executeAndFetch(Students.class);
+        }
+    }
 }
